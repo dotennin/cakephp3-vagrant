0d1bdfcb9abe88f3eed9b8b1709d328cf7388038
nochi0105 <wenhua4438@gmail.com>
2017-12-13 14:12:26 +0900

    Fixed ansible required python2.6 bug

--- .provisioning/provision.sh
+++ .provisioning/provision.sh
@@ -15,11 +15,9 @@ if [ ! -f /vagrant/$ANSIBLE_HOSTS ]; then
 fi
  
 if ! [ `which ansible` ]; then
- wget http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
- sudo rpm --import RPM-GPG-KEY-EPEL-6
- wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
- sudo rpm -ivh epel-release-6-8.noarch.rpm
+ sudo yum -y install epel-release
  sudo yum install -y ansible
+ ansible --version
 fi
  
 cp /vagrant/${ANSIBLE_HOSTS} ${TEMP_HOSTS}
